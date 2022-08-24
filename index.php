fb6511eb584886d34d40a9200f43c5722aac55df
Antonius131 <antomendetta@gmail.com>
2022-08-24 15:11:47 +0200

    creating navbar markup

--- index.php
+++ index.php
@@ -9,5 +9,29 @@
    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
 </head>
 <body>
+   <nav class="navbar navbar-expand-lg border-bottom">
+   <div class="container-fluid">
+      <a class="navbar-brand" href="#">Logo</a>
+      <div class="collapse navbar-collapse" id="navbarNav">
+         <ul class="navbar-nav">
+            <li class="nav-item">
+               <a class="nav-link" href="#">Introduzione</a>
+            </li>
+            <li class="nav-item">
+               <a class="nav-link" href="#">Norme sulla privacy</a>
+            </li>
+            <li class="nav-item">
+               <a class="nav-link" href="#">Termini di servizio</a>
+            </li>
+            <li class="nav-item">
+               <a class="nav-link" href="#">Tecnologie</a>
+            </li>
+            <li class="nav-item">
+               <a class="nav-link active text-primary" aria-current="page" href="#">Domande frequenti</a>
+            </li>
+         </ul>
+      </div>
+   </div>
+   </nav>
 </body>
 </html>
